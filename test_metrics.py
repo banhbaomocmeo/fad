7a6d74d016df27abffc91c5d0a8a8c979f38101b
sangdv <dvietsang@gmail.com>
2018-12-11 08:24:19 +0700

    2

---
+++ test_metrics.py
@@ -0,0 +1,11 @@
+from metrics import ultimate_metrics 
+import numpy as np 
+
+y = np.load('pred.npy')
+# print(y)
+# print(y.shape)
+# print(max(y[1]), min(y[1]))
+# print(y[0][np.where(y[0]==1)].shape[0])
+# print(y[0][np.where(y[0]==0)].shape[0])
+sn, sp, acc, mcc, auc, fpr, tpr = ultimate_metrics(y[0], y[1])
+print ('sn: {}\nsp: {}\nacc: {}\nmcc: {}\nauc: {}\nfpr: {}\ntpr: {}'.format(sn, sp, acc, mcc, auc, fpr, tpr))
