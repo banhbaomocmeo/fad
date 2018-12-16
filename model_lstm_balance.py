c4844ad0923e5c1ebf6509e0c875b52d6c526ec8
le duc anh <banhbaomocmeo@gmail.com>
2018-12-16 08:00:24 +0700

    seed

--- model_lstm_balance.py
+++ model_lstm_balance.py
@@ -17,9 +17,9 @@ from tensorboard import TrainValTensorBoard
 from data import DataGenerator
 import time
 
-seed = 1997
 kfold_splits = 5
 batch_size = 32
+seeds = [25,12,30,1,1997]
 
 def normalize_data(x):
     return 1 / (1 + np.exp(-x))
@@ -47,46 +47,46 @@ X = normalize_data(X)
 X_test_r = normalize_data(X_test_r)
 y_test = to_categorical(y_test, num_classes=2)
 
+for seed in seeds:
+    for index, (train_indices, val_indices) in enumerate(skf.split(X, y)):
+        print ("Training on fold " + str(index+1) + "/5...")
 
-for index, (train_indices, val_indices) in enumerate(skf.split(X, y)):
-    print ("Training on fold " + str(index+1) + "/5...")
+        # Generate batches from indices
+        X_train_r, X_val_r = X[train_indices], X[val_indices]
+        y_train, y_val = (to_categorical(y[train_indices], num_classes=2), to_categorical(y[val_indices], num_classes=2))
+        
+        train_gen = DataGenerator(X_train_r, y_train, batch_size)
 
-    # Generate batches from indices
-    X_train_r, X_val_r = X[train_indices], X[val_indices]
-    y_train, y_val = (to_categorical(y[train_indices], num_classes=2), to_categorical(y[val_indices], num_classes=2))
-    
-    train_gen = DataGenerator(X_train_r, y_train, batch_size)
+        #build
+        model = Sequential()
+        init = glorot_uniform(seed=seed)
+        reg = l2(0.001)
 
-    #build
-    model = Sequential()
-    init = glorot_uniform(seed=seed)
-    reg = l2(0.001)
+        model.add(LSTM(units=100, recurrent_dropout=0.2, dropout=0.2, kernel_initializer=init, input_shape=[17,20]))
 
-    model.add(LSTM(units=100, recurrent_dropout=0.2, dropout=0.2, kernel_initializer=init, input_shape=[17,20]))
+        model.add(Dense(2, kernel_initializer=init, kernel_regularizer=reg))
+        model.add(Activation('softmax'))
 
-    model.add(Dense(2, kernel_initializer=init, kernel_regularizer=reg))
-    model.add(Activation('softmax'))
+        tbCallBack = TrainValTensorBoard(log_dir='./lstm_balance/seed_{}/fold_{}/logs'.format(seed, index), histogram_freq=0, write_graph=True)
+        ckptCallBack =  ModelCheckpoint(filepath='./lstm_balance/seed_{}/fold_{}/logs/best.h5'.format(seed, index), monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+        model.compile(loss='categorical_crossentropy',optimizer=Adam(),metrics=['accuracy'])
+        model.summary()
 
-    tbCallBack = TrainValTensorBoard(log_dir='./lstm_balance/seed_{}/fold_{}/logs'.format(seed, index), histogram_freq=0, write_graph=True)
-    ckptCallBack =  ModelCheckpoint(filepath='./lstm_balance/seed_{}/fold_{}/logs/best.h5'.format(seed, index), monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    model.compile(loss='categorical_crossentropy',optimizer=Adam(),metrics=['accuracy'])
-    model.summary()
+        nb_epoch = 20
+        model.fit_generator(train_gen,  
+                epochs=nb_epoch,
+                validation_data=(X_val_r, y_val), 
+                callbacks=[tbCallBack, ckptCallBack], 
+                class_weight={0: 0.4, 1: 0.6}
+                )
 
-    nb_epoch = 20
-    model.fit_generator(train_gen,  
-            epochs=nb_epoch,
-            validation_data=(X_val_r, y_val), 
-            callbacks=[tbCallBack, ckptCallBack], 
-            class_weight={0: 0.4, 1: 0.6}
-            )
+        #metrics
+        y_pred = model.predict(X_test_r)
+        np.save('./lstm_balance/seed_{}/fold_{}/pred.npy'.format(seed, index), [y_test, y_pred])
 
-    #metrics
-    y_pred = model.predict(X_test_r)
-    np.save('./lstm_balance/seed_{}/fold_{}/pred.npy'.format(seed, index), [y_test, y_pred])
-
-    #save model
-    model_json = model.to_json()
-    with open("./lstm_balance/seed_{}/fold_{}/model.json".format(seed, index), "w") as json_file:
-        json_file.write(model_json)
-    model.save_weights("./lstm_balance/seed_{}/fold_{}/model.h5".format(seed, index))
-    print("Saved model to disk")
+        #save model
+        model_json = model.to_json()
+        with open("./lstm_balance/seed_{}/fold_{}/model.json".format(seed, index), "w") as json_file:
+            json_file.write(model_json)
+        model.save_weights("./lstm_balance/seed_{}/fold_{}/model.h5".format(seed, index))
+        print("Saved model to disk")
