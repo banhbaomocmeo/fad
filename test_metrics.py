3d8213b5a5220efd339a17f05c2e14a9c5f7aece
sangdv <dvietsang@gmail.com>
2018-12-16 08:01:15 +0700

    test

--- test_metrics.py
+++ test_metrics.py
@@ -1,11 +1,16 @@
 from metrics import ultimate_metrics 
 import numpy as np 
 
-y = np.load('lstm/pred.npy')
+ms = ['lstm', 'bi', 'cnn']
+
+for m in ms:
+
+  y = np.load('{}_balance/seed_1997/fold_4/pred.npy'.format(m))
 # print(y)
 # print(y.shape)
 # print(max(y[1]), min(y[1]))
 # print(y[0][np.where(y[0]==1)].shape[0])
 # print(y[0][np.where(y[0]==0)].shape[0])
-sn, sp, acc, mcc, auc, fpr, tpr = ultimate_metrics(y[0], y[1])
-print ('sn: {}\nsp: {}\nacc: {}\nmcc: {}\nauc: {}\nfpr: {}\ntpr: {}'.format(sn, sp, acc, mcc, auc, fpr, tpr))
+  sn, sp, acc, mcc, auc, fpr, tpr = ultimate_metrics(y[0], y[1])
+  print('>>>>>{}<<<<<'.format(m))
+  print ('sn: {}\nsp: {}\nacc: {}\nmcc: {}\nauc: {}\nfpr: {}\ntpr: {}'.format(sn, sp, acc, mcc, auc, fpr, tpr))
