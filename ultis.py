0ade330d338a6965457f1f2ef938e81ba1a86a97
le duc anh <banhbaomocmeo@gmail.com>
2018-12-19 16:31:43 +0700

    test new metrics

---
+++ ultis.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+
+def normalize_data(x):
+    return 1 / (1 + np.exp(-x))
+
+def as_keras_metric(method):
+    import functools
+    from keras import backend as K
+    import tensorflow as tf
+    @functools.wraps(method)
+    def wrapper(self, args, **kwargs):
+        """ Wrapper for turning tensorflow metrics into keras metrics """
+        value, update_op = method(self, args, **kwargs)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([update_op]):
+            value = tf.identity(value)
+        return value
+    return wrapper
