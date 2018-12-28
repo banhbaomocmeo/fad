fc82eb208c107a905b77558d23d4279fc894c9c8
le duc anh <banhbaomocmeo@gmail.com>
2018-12-28 11:49:22 +0700

    update

--- model_bi_for_test.py
+++ model_bi_for_test.py
@@ -19,7 +19,7 @@ import time
 from sklearn.model_selection import train_test_split
 
 batch_size = 32
-nb_epoch = 30
+nb_epoch = 20
 
 
 #config
@@ -30,8 +30,6 @@ keras.backend.set_session(sess)
 
 #metric
 auc = as_keras_metric(tf.metrics.auc)
-recall = as_keras_metric(tf.metrics.recall)
-precision = as_keras_metric(tf.metrics.precision)
 f1_score = as_keras_metric(tf.contrib.metrics.f1_score)
 
 #data
@@ -54,21 +52,21 @@ model = Sequential()
 model.add(Bidirectional(LSTM(units=100, return_sequences=True, recurrent_dropout=0.15, dropout=0.25), input_shape=[17,20]))
 model.add(GlobalMaxPool1D())
 model.add(Dense(100, activation="relu"))
-model.add(Dropout(0.25))
+model.add(Dropout(0.5))
 model.add(Dense(2))
 model.add(Activation('softmax'))
-model.compile(loss='categorical_crossentropy',optimizer=RMSprop(),metrics=['accuracy', auc, recall, precision, f1_score])
+model.compile(loss='categorical_crossentropy',optimizer=RMSprop(),metrics=['accuracy', auc, f1_score])
 model.summary()
 
 tbCallBack = TrainValTensorBoard(log_dir='./bi_test/logs', histogram_freq=0, write_graph=True)
-ckptCallBack =  ModelCheckpoint(filepath='./bi_test/best.h5', monitor='val_auc', verbose=1, save_best_only=True, mode='max')
-reduceLRCallBack = ReduceLROnPlateau(monitor='val_f1_score', factor=0.3, patience=5, verbose=0, min_lr=1e-6)
+ckptCallBack =  ModelCheckpoint(filepath='./bi_test/best.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='max')
+reduceLRCallBack = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=5, verbose=0, min_lr=1e-6)
 
 model.fit_generator(train_gen,  
         epochs=nb_epoch,
         validation_data=(X_test, y_test), 
         callbacks=[tbCallBack, ckptCallBack, reduceLRCallBack], 
-        #class_weight={0: 0.4, 1: 0.6}
+        class_weight={0: 0.8, 1: 0.2}
         )
 
 #save model
